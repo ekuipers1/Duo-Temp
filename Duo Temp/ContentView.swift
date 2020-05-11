5d6486950ccead9feefcf736671d2da261ff180d
Erik Kuipers <ekuipers1@gmail.com>
2020-05-11 12:37:59 +0200

    Initial Commit

---
+++ Duo Temp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Duo Temp
+//
+//  Created by Erik Kuipers on 5/11/20.
+//  Copyright © 2020 Erik Kuipers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
